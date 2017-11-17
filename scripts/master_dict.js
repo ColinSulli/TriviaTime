9e12674afb3cce3eedeb21d98912125f4abbcb64
Jason Pinho <jpinho@umich.edu>
2017-11-16 20:29:04 -0500

    can spawn images and added animation

---
+++ scripts/master_dict.js
@@ -0,0 +1,20 @@
+
+var master_dict = {
+    piano: { 
+        0: ["./img/batman_run.gif", "./audio/gameover.wav"]
+
+
+
+    }//end piano
+
+
+    // orchestra: {
+
+
+
+
+    // }
+
+
+
+}
--- scripts/music_maker.js
+++ scripts/music_maker.js
@@ -1,14 +1,14 @@
 // ==================== GLOBAL VARS ==================== //
 
-// Counters
+// counters
+var img_idx = 0;
 
-
-// Size Constants
+// size and movement constants
 var OBJECT_SPEED = 5;           // pixels per ms
 var OBJECT_REFRESH_RATE = 50;   // ms
 
-var state = "initial";         // options: "initial" or "running"
-
+var state = "initial";          // options: "initial" or "running"
+var mode  = "piano";            //FIXME
 
 
 
@@ -35,18 +35,54 @@ function keydownRouter(e) {
         state = "running";
     }//end if
 
-    e.preventDefault();
+    var keypressed = KEYS[e.which];
+    if (keypressed === "escape") {
+        exit();
+        return;
+    }//end if
 
-    switch (e.which) {
-        case KEYS.escape:
-            exit();
-            break;
-        default:
-            break;
-    }
+    console.log("key   =", keypressed);
+    console.log("image =", master_dict[mode][keypressed][0]);
+    create_image(master_dict[mode][keypressed][0]);
+    // master_dict[mode][keypressed][1].play();
+
 }//end keydownRouter()
 
 
+function create_image(image_src) {
+    var curr_img_id = "img-" + img_idx;
+    var curr_image_div = "<div id='" + curr_img_id + "' class='images'></div>";
+    $('.main').append(curr_image_div);
+    img_idx++;
+
+    $("#" + curr_img_id).append("<img src='" + image_src + "' height='" + "100px" + "'/>")
+
+
+    animate_top_down(curr_img_id);
+
+}//end create_image()
+
+
+// image_id should be passed as   var curAlien = $('#a-' + alienIdx);
+function animate_top_down(image_id) {
+    var this_img = $('#' + image_id);
+
+    // set where image start horizontally
+    var starting_position = Math.random() * ($(".main").width() - this_img.width());
+    this_img.css("left", starting_position + "px");
+
+    var img_animate = setInterval( function() {
+        this_img.css("top", parseInt(this_img.css('top')) + OBJECT_SPEED);
+        // console.log("inside setInterval");
+        // console.log("curr css top =", this_img.css('top'));
+        // console.log("main height =", $('.main').height());
+        // Check to see if the image has left the game/viewing window
+        if (parseInt(this_img.css('top')) > ($('.main').height() - this_img.height())) {
+            this_img.remove();
+            clearInterval(img_animate);
+        }//end if
+    }, OBJECT_REFRESH_RATE);
+}//end animate_top_down()
 
 
 function exit() {
@@ -65,5 +101,5 @@ function exit() {
 function get_random_num(min, max) {
     // borrowed from Stack Overflow
     // https://stackoverflow.com/questions/4959975/generate-random-number-between-two-numbers-in-javascript
-    return Math.random() * (max - min) + min;
+    return Math.floor(Math.random() * (max - min) + min);
 }//end get_random_num()
