7b943f127a91e1349b6f936e0722828400c2ba73
Jason Pinho <jpinho@umich.edu>
2017-12-07 18:06:17 -0500

    added skeleton js file and screenshots

---
+++ website/scripts/website.js
@@ -0,0 +1,24 @@
+// ==================== GLOBAL VARS ==================== //
+
+
+
+
+
+
+
+// ==================== MAIN CODE ==================== //
+
+$(document).ready( function() {
+    console.log("Ready!");
+
+    
+
+});//end document.ready()
+
+// ==================== CALLBACK FUNCTIONS ==================== //
+
+function FIXME() {
+
+
+
+}
